a1c5a11beb3b3d6bd88f65a7c5458e90ee16a96c
sachhayaKO <sachhaya1@gmail.com>
2025-07-09 10:52:50 -0400

    File Reading Testing with pyfaidx and SeqIO

--- data/faidx.py
+++ data/faidx.py
@@ -1,11 +1,5 @@
-from Bio import SeqIO, Seq, SeqRecord, os
-
+from pyfaidx import Fasta, os
 project_root = os.path.dirname(os.path.dirname(__file__)) #Gets the folder containing the script
 fasta_path = os.path.join(project_root, "data", "sequence.fasta") #Creates a path to the fasta file
-my_seq = SeqIO.read(fasta_path, "fasta")
-
-
-
-
-
-
+genes = Fasta(fasta_path)
+print(genes.keys())
